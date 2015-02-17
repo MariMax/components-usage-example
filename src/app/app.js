afdd822164e1dd43ea77cc95bf4602517f0b5213
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-02-17 23:23:58 +0300

    Use static component

--- src/app/app.js
+++ src/app/app.js
@@ -4,6 +4,8 @@ var React = require('react');
 var PlainJsComponent = require('plain-js/plain-js');
 var SomeJsx = require('some-jsx/some-jsx.jsx');
 
+require('static/static');
+
 var RenderExampleComponents = function () {
     var plain = new PlainJsComponent();
     plain.renderInto(document.getElementById('app__awesome'));
