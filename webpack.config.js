2b0a0a91db197fb4230290b619204b499fc6682f
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-02-17 22:15:49 +0300

    Set up building

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpackConfigMerger = require('webpack-config-merger');
+
+module.exports = webpackConfigMerger(require('components-example/webpack.config'), {
+    entry: './src/app/app.js',
+    output: {
+        path: './dist',
+        filename: '[name].js',
+        library: '[name]',
+        libraryTarget: 'umd'
+    },
+    module: {
+        preLoaders: [
+            {
+                test:  /.*\/app\/.*\.js$/,
+                loader: 'jshint-loader'
+            }
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+});
