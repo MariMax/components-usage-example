19d6fbb70a201ffff3776432fbb2b06f224edd43
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-02-26 21:41:45 +0300

    Rename plain component

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,13 @@
 require('./app.css');
 var React = require('react');
 
-var PlainJsComponent = require('plain-js/plain-js');
+var Plain = require('plain-js/plain-js');
 var SomeJsx = require('some-jsx/some-jsx.jsx');
 
 require('static/static');
 
 var RenderExampleComponents = function () {
-    var plain = new PlainJsComponent(document.getElementById('app__plain-js'));
+    var plain = new Plain(document.getElementById('app__plain-js'));
 
     React.render(
         React.createElement(SomeJsx, null),
